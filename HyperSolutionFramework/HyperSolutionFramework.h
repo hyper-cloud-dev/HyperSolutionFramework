f9dd1d49ce0ac02e550bac9242af96bba5dbdb4f
윤창현 <gus@hyper-cloud.kr>
2023-03-21 12:22:20 +0900

    Initial Commit

---
+++ HyperSolutionFramework/HyperSolutionFramework.h
@@ -0,0 +1,18 @@
+//
+//  HyperSolutionFramework.h
+//  HyperSolutionFramework
+//
+//  Created by changhyen yun on 2023/03/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for HyperSolutionFramework.
+FOUNDATION_EXPORT double HyperSolutionFrameworkVersionNumber;
+
+//! Project version string for HyperSolutionFramework.
+FOUNDATION_EXPORT const unsigned char HyperSolutionFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HyperSolutionFramework/PublicHeader.h>
+
+
